0edaef6060a511571382ea50296cf2dd2d0692a5
ulia <sivonenkoyulia@gmail.con>
2020-10-24 16:23:27 +0300

    init

---
+++ Desktop/Java script/home work/hw-js-11-color-switch/src/js/swith.js
@@ -0,0 +1,41 @@
+import colors from './color.js';
+
+
+const refs = {
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+  body: document.querySelector('body'),
+  buttons: document.getElementsByTagName('button'),
+};
+
+let colorId = null;
+
+refs.startBtn.addEventListener('click', changeColorHandler);
+refs.stopBtn.addEventListener('click', stopChangingHandler);
+
+function changeColorHandler() {
+  activeButtonHandler(refs.stopBtn, refs.startBtn);
+
+  colorId = setInterval(() => {
+    return runRandomColor();
+  }, 1000);
+}
+
+function stopChangingHandler() {
+  activeButtonHandler(refs.startBtn, refs.stopBtn);
+  clearInterval(colorId);
+}
+
+function runRandomColor() {
+  const swithcColor = randomIntegerFromInterval(0, colors.length - 1);
+  refs.body.style.backgroundColor = colors[swithcColor];
+}
+
+function activeButtonHandler(enabled, disabled) {
+  disabled.setAttribute('disabled', 'disabled');
+  enabled.removeAttribute('disabled');
+}
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
