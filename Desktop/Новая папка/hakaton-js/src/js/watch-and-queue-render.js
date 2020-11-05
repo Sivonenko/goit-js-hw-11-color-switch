1893283dcd6ceb0bba96fb9616050d17c87a8a73
ulia <sivonenkoyulia@gmail.con>
2020-11-05 19:37:07 +0200

    error

---
+++ Desktop/Новая папка/hakaton-js/src/js/watch-and-queue-render.js
@@ -0,0 +1,9 @@
+// import watchQueueSectionTpl from '../templates/library-section.hbs';
+
+// function createWatchAndQueueSectionMarkup() {
+//   const markupWatchQueueSection = watchQueueSectionTpl();
+//   const containerRef = document.querySelector('.container');
+
+//   containerRef.insertAdjacentHTML('beforeend', markupWatchQueueSection);
+// }
+
